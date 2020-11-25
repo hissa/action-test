124a4b0cac2c929d136078a03362a0a93a91202b
Hisada Masanari <hissa@users.noreply.github.com>
2020-11-25 12:01:21 +0900

    Add test

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . import models
 
 # Create your tests here.
+
+
+class StudentTest(TestCase):
+    def test_to_str(self):
+        self.assertEqual(models.Student.objects.count(), 0)
+        student = models.Student(name="TestTaro")
+        student.save()
+        self.assertEqual(models.Student.objects.count(), 1)
+        self.assertEqual(str(student), f'{student.id}: {student.name}')
